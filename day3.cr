aed28ade43541448971b54bfe4290f293d4946cb
Linus Sellberg <sellberg@gmail.com>
2020-12-03 10:53:08 +0100

    Small cleanup day 3.

--- day3.cr
+++ day3.cr
@@ -3,20 +3,18 @@ inputs = File
   .split
 
 def trees(inputs, right, down)
-  y = 0
+  y = -right
   row_size = inputs[0].size
-  trees = 0i64
-  0.step(to: inputs.size - 1, by: down) do |x|
-    trees += 1 if inputs[x][y] == '#'
+  0.step(to: inputs.size - 1, by: down).count do |x|
     y += right
     y -= row_size if y >= row_size
+    inputs[x][y] == '#'
   end
-  trees
 end
 
 puts "part1: #{trees(inputs, 3, 1)}"
 
 paths = { {1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2} }
-  .map { |right, down| trees(inputs, right, down) }
+  .product(1i64) { |right, down| trees(inputs, right, down) }
 
-puts "part2: #{paths.product}"
+puts "part2: #{paths}"
